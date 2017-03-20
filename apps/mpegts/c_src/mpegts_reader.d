69439f1f3da254845dec97fc32a32539c8d73bf4
Yitzchok <yitzchok@ychakiris-Inspiron-560>
2017-03-20 11:42:04 -0500

    Many small changes to get it to compile. Mainly, depreciated functions such as erlang:now(), random functions now in module rand, and dealing with the parameterized modules by using pmod_transform. In addition some small changes in the rebar config's for c_src where the nif_init were confusing the orginal config files.

---
+++ apps/mpegts/c_src/mpegts_reader.d
@@ -0,0 +1,5 @@
+c_src/mpegts_reader.o: c_src/mpegts_reader.c \
+ /home/yitzchok/19.0/erts-8.0/include/erl_nif.h \
+ /home/yitzchok/19.0/erts-8.0/include/erl_drv_nif.h \
+ /home/yitzchok/19.0/erts-8.0/include/erl_int_sizes_config.h \
+ /home/yitzchok/19.0/erts-8.0/include/erl_nif_api_funcs.h
